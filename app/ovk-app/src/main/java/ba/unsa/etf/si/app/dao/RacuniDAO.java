dc494fae01fc0fc5fa27ca6bb8dd89404743ddb4
Kenan Mahmutovic <kmahmutovi1@etf.unsa.ba>
2015-05-20 22:34:41 +0200

    Doradjen malo servis

--- app/ovk-app/src/main/java/ba/unsa/etf/si/app/dao/RacuniDAO.java
+++ app/ovk-app/src/main/java/ba/unsa/etf/si/app/dao/RacuniDAO.java
@@ -2,9 +2,12 @@ package ba.unsa.etf.si.app.dao;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
+import ba.unsa.etf.si.app.entity.Korisnik;
 import ba.unsa.etf.si.app.entity.Parametri;
 import ba.unsa.etf.si.app.entity.Racuni;
 
@@ -18,7 +21,15 @@ public class RacuniDAO extends AbstractDAO<Racuni,Integer> {
 	}
 	public List<Racuni> PreuzmiSveRacune(){
 		return null;
-	}	
+	}
+	
+	public List<Racuni> pretragaRacuna(int sifra, int potrosac, String ime, String prezime) {  
+        Criteria criteria = getSession().createCriteria(getPersistentClass());
+        criteria.add(Restrictions.like("ime","%" + ime + "%").ignoreCase());
+        criteria.add(Restrictions.like("prezime","%" + prezime + "%").ignoreCase());
+        criteria.add(Restrictions.like("id",sifra).ignoreCase());
+        return criteria.list();  
+    }  
 	
 	public void closeSession(){
 		session.close();
--- app/ovk-app/src/main/java/ba/unsa/etf/si/app/services/ObracunService.java
+++ app/ovk-app/src/main/java/ba/unsa/etf/si/app/services/ObracunService.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.si.app.services;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -7,7 +9,15 @@ import org.hibernate.Session;
 
 
 
+
+
+
+
+import org.hibernate.Transaction;
+
+import ba.unsa.etf.si.app.dao.OcitanjaDAO;
 import ba.unsa.etf.si.app.dao.RacuniDAO;
+import ba.unsa.etf.si.app.entity.Ocitanja;
 import ba.unsa.etf.si.app.entity.Parametri;
 import ba.unsa.etf.si.app.entity.Racuni;
 import ba.unsa.etf.si.app.util.HibernateUtil;
@@ -20,22 +30,33 @@ public class ObracunService {
 	
 	public ObracunService(){
 		 dao = new RacuniDAO();
+		 Parametri parametri = parametriService.DajParametre();
+		 ObracunService.setCijenaKanalizacijePoKubiku(parametri.getCijenaKanalizacijePoKubiku());
+		 ObracunService.setCijenaVodePoKubiku(parametri.getCijenaVodePoKubiku());
+		 ObracunService.setFiksnaCijena(parametri.getFiksnaCijena());
+		 ObracunService.setPVNKoristenja(parametri.getPvnZaKoristenjeVoda());
+		 ObracunService.setPVNZastite(parametri.getPvnZaZastituVoda());
+		 ObracunService.setStopaPDV(parametri.getStopaPdv());
+		 
 	}
 
-		public static double getCijenaProizvodnje() {
-		return cijenaProizvodnje;
+
+
+		public static double getCijenaVodePoKubiku() {
+		return cijenaVodePoKubiku;
 	}
 
-	public static void setCijenaProizvodnje(double cijenaProizvodnje) {
-		ObracunService.cijenaProizvodnje = cijenaProizvodnje;
+	public static void setCijenaVodePoKubiku(double cijenaVodePoKubiku) {
+		ObracunService.cijenaVodePoKubiku = cijenaVodePoKubiku;
 	}
 
-	public static double getCijenaOdvodjenja() {
-		return cijenaOdvodjenja;
+	public static double getCijenaKanalizacijePoKubiku() {
+		return cijenaKanalizacijePoKubiku;
 	}
 
-	public static void setCijenaOdvodjenja(double cijenaOdvodjenja) {
-		ObracunService.cijenaOdvodjenja = cijenaOdvodjenja;
+	public static void setCijenaKanalizacijePoKubiku(
+			double cijenaKanalizacijePoKubiku) {
+		ObracunService.cijenaKanalizacijePoKubiku = cijenaKanalizacijePoKubiku;
 	}
 
 	public static double getPVNKoristenja() {
@@ -54,12 +75,22 @@ public class ObracunService {
 		PVNZastite = pVNZastite;
 	}
 
-	public static double getFiksniBrojKubika() {
-		return fiksniBrojKubika;
+	public static double getFiksniBrojKubikaVodaZaPausalce() {
+		return fiksniBrojKubikaVodaZaPausalce;
 	}
 
-	public static void setFiksniBrojKubika(double fiksniBrojKubika) {
-		ObracunService.fiksniBrojKubika = fiksniBrojKubika;
+	public static void setFiksniBrojKubikaVodaZaPausalce(
+			double fiksniBrojKubikaVodaZaPausalce) {
+		ObracunService.fiksniBrojKubikaVodaZaPausalce = fiksniBrojKubikaVodaZaPausalce;
+	}
+
+	public static double getFiksniBrojKubikaKanalizacijaZaPausalce() {
+		return fiksniBrojKubikaKanalizacijaZaPausalce;
+	}
+
+	public static void setFiksniBrojKubikaKanalizacijaZaPausalce(
+			double fiksniBrojKubikaKanalizacijaZaPausalce) {
+		ObracunService.fiksniBrojKubikaKanalizacijaZaPausalce = fiksniBrojKubikaKanalizacijaZaPausalce;
 	}
 
 	public static double getStopaPDV() {
@@ -70,26 +101,97 @@ public class ObracunService {
 		ObracunService.stopaPDV = stopaPDV;
 	}
 
-		public static double cijenaProizvodnje;
-		public static double cijenaOdvodjenja;
+	public static double getFiksnaCijena() {
+		return fiksnaCijena;
+	}
+
+	public static void setFiksnaCijena(double fiksnaCijena) {
+		ObracunService.fiksnaCijena = fiksnaCijena;
+	}
+
+
+
+		public static double cijenaVodePoKubiku;
+		public static double cijenaKanalizacijePoKubiku;
 		public static double PVNKoristenja;
 		public static double PVNZastite;
-		public static double fiksniBrojKubika;
+		public static double fiksniBrojKubikaVodaZaPausalce;
+		public static double fiksniBrojKubikaKanalizacijaZaPausalce;
 		public static double stopaPDV;
+		public static double fiksnaCijena;
 		
 		
 		
-		public void formirajRacune(Date pocetni, Date krajnji){
+		public List<Racuni> formirajRacune(Calendar pocetni, Calendar krajnji){
+			 List<Racuni> racuni = new ArrayList<Racuni>();
+			 OcitanjaService servis = new OcitanjaService();
+			 List<Ocitanja> ocitanja = servis.pretragaOcitanja(pocetni.MONTH, pocetni.YEAR);
+			  
+			 
+			 for(int i=0;i<ocitanja.size();i++){
+				 Ocitanja o = servis.pretragaOcitanja(ocitanja.get(i).getPotrosacByIdPotrosaca().getId().intValue(),krajnji.MONTH, pocetni.YEAR);
+				 double vrijednost = o.getPotrosnja() - ocitanja.get(i).getPotrosnja();
+				 Racuni racun = obracunajRacun(vrijednost);
+				 racun.setPotrosac(ocitanja.get(i).getPotrosacByIdPotrosaca());
+				 racun.setOcitanja(ocitanja.get(i));
+				 			 
+				 racuni.add(racun);
+				 				 
+			 }
+			
+			 return racuni;
+		}
+		
+		
+		
+		private Racuni obracunajRacun(double vrijednost) {
+			 Racuni racun = new Racuni();
+			 double cijenaKanalizacije = vrijednost*cijenaKanalizacijePoKubiku;
+			 double cijenaVode = vrijednost*cijenaVodePoKubiku;
+			 double cijenaKanalizacijeSaPDV = cijenaKanalizacije + cijenaKanalizacije*stopaPDV;
+			 double cijenaVodeSaPDV = cijenaVode + cijenaVode*stopaPDV;
+			 double ukupnaCijena = (cijenaVodePoKubiku+cijenaKanalizacijePoKubiku+PVNKoristenja+PVNZastite)*vrijednost+fiksnaCijena;
+			 double ukupnaCijenaSaPDV = ukupnaCijena+ ukupnaCijena*stopaPDV;
+			 // double cijenaZaKoristenjeVode = vrijednost*PVNKoristenja;
+			 racun.setCijenaKanalizacije(cijenaKanalizacije);
+			 racun.setCijenaKanalizacijeSaPdv(cijenaKanalizacijeSaPDV);
+			 racun.setCijenaVoda(cijenaVode);
+			 racun.setCijenaVodaSaPdv(cijenaVodeSaPDV);
+			 racun.setFisknaCijenaZaKoristenjeUsluga(fiksnaCijena);
+			 racun.setPotrosnjaZaKoristenjeKanalizacije(vrijednost);
+			 racun.setPotrosnjaZaKoristenjeVoda(vrijednost);
+			 racun.setPvnZaKoristenjeVoda(PVNKoristenja);
+			 racun.setPvnZaZastituVoda(PVNZastite);
+			 racun.setUkupnaCijena(ukupnaCijena);
+			 racun.setUkupnaCijenaSaPdv(ukupnaCijenaSaPDV);
+			 
+			 return racun;
+			 
 			 
 		}
-		
-		public boolean snimiRacune(){
-			return true;
-		}
-		public void printajRacune(){
+
+
+
+		public boolean snimiRacune(List<Racuni> racuni){
+			try{
+				s = HibernateUtil.getSessionFactory().openSession();
+				Transaction t = s.beginTransaction();
+				for( int i=0; i< racuni.size(); i++){
+				s.save(racuni.get(i));
+				
+			}
+				t.commit();
+				s.close();
+				return true;
+				
+				}
+				catch(Exception e){
+					return false;
+				}
+				
 			
 		}
-		
+				
 		public boolean modificirajRacun(Racuni racun){
 			return true;
 		}
