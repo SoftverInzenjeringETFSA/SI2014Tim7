0da9b831b09896e727259b26c290dec98fc55fed
Anes Luckin <aluckin1@etf.unsa.ba>
2015-05-24 04:03:58 +0200

    OcitanjaService rjesenje moga i Kenanov problema

--- app/ovk-app/src/main/java/ba/unsa/etf/si/app/services/OcitanjaService.java
+++ app/ovk-app/src/main/java/ba/unsa/etf/si/app/services/OcitanjaService.java
@@ -3,40 +3,45 @@ package ba.unsa.etf.si.app.services;
 
 import ba.unsa.etf.si.app.dao.OcitanjaDAO;
 import ba.unsa.etf.si.app.dao.PotrosacDAO;
-
 import ba.unsa.etf.si.app.entity.Ocitanja;
 import ba.unsa.etf.si.app.entity.Potrosac;
 import ba.unsa.etf.si.app.util.HibernateUtil;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 
+
+
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 
 
 public class OcitanjaService{
-	
+		private Session session;
+		
+		public OcitanjaService()
+		{
+			session = HibernateUtil.getSessionFactory().openSession();
+		}
     
     public Potrosac getPotrosac(int sifra){
-        Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         PotrosacDAO dao = new PotrosacDAO();
         dao.setSession(session);
         List<Potrosac> pList = dao.findByFullSifra(sifra);
         if(pList.isEmpty()){
         	session.getTransaction().commit();
-            session.close();
             throw new IllegalArgumentException("Potrosac sa datom sifrom ne postoji");
         }
         else{
         	
-        session.getTransaction().commit();
-        session.close();	
+        session.getTransaction().commit();	
         return pList.get(0);
         }
     }
     public void createNewOcitanja(Ocitanja o){
-        Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         OcitanjaDAO dao = new OcitanjaDAO();
         dao.setSession(session);
@@ -46,15 +51,13 @@ public class OcitanjaService{
         Potrosac temp = pService.getPotrosacByJMBG(p.getJmbg());
         if(temp.getJmbg() == null ? p.getJmbg() != null : !temp.getJmbg().equals(p.getJmbg())){
             session.getTransaction().commit();
-            session.close();
             throw new IllegalArgumentException("Potrosac kojeg trazite ne postoji u bazi podataka");
         }
         else {
             List<Ocitanja> tempOcitanjaList = dao.findOcitanja(o.getMjesec(), o.getGodina(), o.getPotrosacByIdPotrosaca());
             if(!tempOcitanjaList.isEmpty()){
                 session.getTransaction().commit();
-                session.close();
-                throw new IllegalArgumentException("Ocitanja za date podatke vec postoji");
+                throw new IllegalArgumentException("Ocitanje za date podatke vec postoji");
             }
             else{
                 // Dozvoliti mogucu izmjenu ocitanja
@@ -62,43 +65,37 @@ public class OcitanjaService{
                 dao.save(o);
                 // Zatvaranje sesije, isto obavezni dio
                 session.getTransaction().commit();
-                session.close();
+
             }
         }
     }
     // Sljedece dvije klase rade zajedno, prvo nadjemo listu ocitanja, izaberemo jednu od opcija, te zatim modificiramo izabrano ocitanje
     public List<Ocitanja> getId(int sifra){
-        Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         OcitanjaDAO dao = new OcitanjaDAO();
         dao.setSession(session);
         List<Ocitanja> tempOcitanjaList = dao.findOcitanjaBySifraVodomjeraAccess(sifra);
         if(tempOcitanjaList.isEmpty()){
         	session.getTransaction().commit();
-            session.close();
-            throw new IllegalArgumentException("Ne postoji ocitanje u bazi sa datim podatcima");
+            throw new IllegalArgumentException("Ne postoji ocitanje u bazi sa datim podacima");
         }
         else{
         	session.getTransaction().commit();
-            session.close();
             return tempOcitanjaList;
         }
     }
     
     public void modifyOcitanja(Ocitanja o){
-        Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         OcitanjaDAO dao = new OcitanjaDAO();
         dao.setSession(session);
         dao.save(o);
         // Zatvaranje sesije, isto obavezni dio
         session.getTransaction().commit();
-        session.close();
     	}
         
     // Sljedeca metoda radi zajedno sa getPotrosac
     public void deleteOcitanja(Ocitanja o){
-        Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         OcitanjaDAO dao = new OcitanjaDAO();
         dao.setSession(session);
@@ -106,26 +103,20 @@ public class OcitanjaService{
         List<Ocitanja> tempOcitanjaList = dao.findOcitanjaForDelete(o.getMjesec(), o.getGodina(), o.getPotrosacByIdPotrosaca());
         if(tempOcitanjaList.isEmpty()){
             session.getTransaction().commit();
-            session.close();
             throw new IllegalArgumentException("Ocitanje za unesene vrijednosti ne postoji ili je racun vec napravljan");
         }
         else{
             dao.delete(tempOcitanjaList.get(0).getId());
             session.getTransaction().commit();
-            session.close();
         }
     }
-    
-    
     // sljedece metode rade uz ObracunService
     public List<Ocitanja> getOcitanjaForRacuni(Date odDatuma,Date doDatuma){
-            Session session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
             OcitanjaDAO dao = new OcitanjaDAO();
             dao.setSession(session);
             List<Ocitanja> ocitanjaZaRacune = dao.findByDate(odDatuma,doDatuma);
             session.getTransaction().commit();
-            session.close();
             if(ocitanjaZaRacune.isEmpty()){
                 throw new IllegalArgumentException("Nema ocitanja u tom periodu");
             }
