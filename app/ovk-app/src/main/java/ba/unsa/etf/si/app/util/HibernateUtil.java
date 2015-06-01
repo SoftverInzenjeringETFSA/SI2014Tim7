774ee97665bf5ce5acb657d142d0a9c218a5a217
aklacar1 <aklacar1@etf.unsa.ba>
2015-06-01 20:24:07 +0200

    DB Properties Configured

--- app/ovk-app/src/main/java/ba/unsa/etf/si/app/util/HibernateUtil.java
+++ app/ovk-app/src/main/java/ba/unsa/etf/si/app/util/HibernateUtil.java
@@ -5,34 +5,35 @@
  */
 package ba.unsa.etf.si.app.util;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-
-//pomocna klasa za kreiranje sesija
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
 
-    static {
-    	
-            try {
-            	
-            	Configuration cfg = new Configuration().configure("hibernate.cfg.xml");        	
-            	StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
-            	sb.applySettings(cfg.getProperties());
-            	StandardServiceRegistry standardServiceRegistry = sb.build();           	
-            	sessionFactory = cfg.buildSessionFactory(standardServiceRegistry);  
-            	
-            } catch (Exception th) {
-            	
-                    System.err.println("Enitial SessionFactory creation failed" + th);
+	private static final SessionFactory sessionFactory;
+	private static final ServiceRegistry serviceRegistry;
+
+	static {
+
+		try {
+			java.util.Properties properties = new Properties();
+			properties.load(new FileInputStream("db.properties"));
+                        Configuration configuration = new Configuration();
+                        configuration.configure().addProperties(properties);
+		    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                        configuration.getProperties()).build();
+		    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		} catch (Exception th) {
+                    System.err.println("Initial SessionFactory creation failed" + th);
                     throw new ExceptionInInitializerError(th);
-            }
-    }
-    
-    public static SessionFactory getSessionFactory() {
-            return sessionFactory;
-    }
+		}
+	}
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 }
